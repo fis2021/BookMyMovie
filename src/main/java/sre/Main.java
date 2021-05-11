098942172812e8e4656f957ea7f1a984a548d3dc
Beatrice Buhai <roberta.b.buhai@gmail.com>
2021-05-11 23:46:38 +0300

    login1

--- src/main/java/sre/Main.java
+++ src/main/java/sre/Main.java
@@ -17,8 +17,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         initDirectory();
         UserService.initDatabase();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
-        primaryStage.setTitle("Registration Example");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        primaryStage.setTitle("Registration");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+        Parent root1 = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
