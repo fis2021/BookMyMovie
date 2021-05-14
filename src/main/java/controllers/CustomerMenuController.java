d0dbcb4a8e90463e21dfa2a586c6e9d82f2d4071
Bran Bianca <biancadenisa93@yahoo.com>
2021-05-14 23:52:06 +0300

    MOVIE-6 #Added CustomerMenu

---
+++ src/main/java/controllers/CustomerMenuController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import services.UserService;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class CustomerMenuController extends ChangeController {
+    @FXML
+
+    public void handleBookSelectedAction(ActionEvent event) throws  IOException {
+        changeScene(event, "booking.fxml");
+    }
+    @FXML
+    public void handleBuySelectedAction(ActionEvent event) throws  IOException{
+        changeScene(event, "buy.fxml");
+    }
+    @FXML
+    public void handleGenreAction(ActionEvent event) throws  IOException{
+        changeScene(event, "search.fxml");
+    }
+    @FXML
+    public void handleLogoutAction(ActionEvent event) throws  IOException{
+        changeScene(event, "login.fxml");
+    }
+
+}
